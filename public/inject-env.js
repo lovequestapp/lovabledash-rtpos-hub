7ee9cfea008a3b5f846fa0e993e980bec32dcbb3
houstoncardinal <hunainqureshicardinal@gmail.com>
2025-09-29 15:18:32 -0500

    Remove exposed secrets and security vulnerabilities
    - Deleted public/inject-env.js that was exposing API keys
    - Removed script reference from index.html
    - Environment variables now properly handled through Vite's import.meta.env
    - No more secrets exposed in client-side code
    - Build passes and deployment should work

--- public/inject-env.js
+++
@@ -1,8 +0,0 @@
-// This script will be injected by Netlify to provide environment variables
-(function() {
-  if (typeof window !== 'undefined') {
-    window.__NETLIFY_ENV__ = {
-      VITE_OPENAI_API_KEY: process.env.VITE_OPENAI_API_KEY
-    };
-  }
-})();
